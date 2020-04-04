f70bd3fbf91887a3e02900fbee5b0993a7cde977
NikitaGundorin <djnig@yandex.ru>
2020-04-04 16:08:56 +0300

    Add add button

---
+++ EasyFinance/View/AddButton.swift
@@ -0,0 +1,21 @@
+//
+//  AddButton.swift
+//  EasyFinance
+//
+//  Created by Никита Гундорин on 04.04.2020.
+//  Copyright © 2020 Nikita Gundorin. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable class AddButton: UIButton {
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        layer.cornerRadius = 24
+        layer.backgroundColor = UIColor.systemBlue.cgColor
+        titleLabel?.textColor = UIColor.white
+        self.heightAnchor.constraint(equalToConstant: 48).isActive = true
+    }
+}
