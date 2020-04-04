5477de9cabab987ec8e5e47675525353aedaa4f5
NikitaGundorin <djnig@yandex.ru>
2020-04-04 17:49:48 +0300

    Add popup for adding income

--- EasyFinance/View/IncomeViewController.swift
+++ EasyFinance/View/IncomeViewController.swift
@@ -13,14 +13,59 @@ class IncomeViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet var dataProvider: IncomeDataProvider!
     @IBOutlet weak var balance: UILabel!
+    @IBOutlet weak var addButton: AddButton!
+    @IBOutlet weak var addTextField: UITextField!
+    @IBOutlet weak var popupView: UIView!
+    @IBOutlet weak var bottomConstraint: NSLayoutConstraint!
     var viewModel: IncomeViewModel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+          NotificationCenter.default.addObserver(self,
+                                                 selector: #selector(keyboardWillShow),
+                                                 name: UIResponder.keyboardWillShowNotification,
+                                                 object: nil)
+        
+          NotificationCenter.default.addObserver(self,
+                                                 selector: #selector(keyboardWillHide),
+                                                 name: UIResponder.keyboardWillHideNotification,
+                                                 object: nil)
+        
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+
+        view.addGestureRecognizer(tap)
         
         viewModel = IncomeViewModel()
         dataProvider.viewModel = viewModel
         balance.text = viewModel.balance
     }
+    
+    @objc func keyboardWillShow(notification: NSNotification) {
+        guard let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue
+            else { return }
+        self.bottomConstraint.constant = keyboardSize.height
+    }
 
+    @objc func keyboardWillHide(notification: NSNotification) {
+        self.bottomConstraint.constant = 0
+    }
+    
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+    
+    @IBAction func addButtonTapped(_ sender: Any) {
+        guard let value = addTextField.text,
+            value != ""
+            else {
+                UIView.animate(withDuration: 0.2) {
+                    self.view.bringSubviewToFront(self.popupView)
+                    self.popupView.backgroundColor = UIColor(white: 0, alpha: 0.5)
+                    self.addTextField.becomeFirstResponder()
+                }
+                return
+            }
+        dismissKeyboard()
+        self.view.bringSubviewToFront(tableView)
+    }
 }
