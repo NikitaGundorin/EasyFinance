3e73452e6dac354a3675160f1d9f8de04579b59f
NikitaGundorin <djnig@yandex.ru>
2020-04-09 15:04:44 +0300

    Add splash screen and app icon

---
+++ EasyFinance/View/SplashViewController.swift
@@ -0,0 +1,31 @@
+//
+//  SplashViewController.swift
+//  EasyFinance
+//
+//  Created by Никита Гундорин on 09.04.2020.
+//  Copyright © 2020 Nikita Gundorin. All rights reserved.
+//
+
+import UIKit
+
+class SplashViewController: UIViewController {
+    
+    @IBOutlet weak var logoImage: UIImageView!
+    @IBOutlet weak var textImage: UIImageView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        textImage.transform = CGAffineTransform(translationX: 0, y: 20)
+        textImage.alpha = 0
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        UIView.animate(withDuration: 0.3, animations: {
+            self.textImage.alpha = 1
+        })
+        UIView.animate(withDuration: 0.6, animations: {
+            self.textImage.transform = .identity
+        })
+    }
+}
